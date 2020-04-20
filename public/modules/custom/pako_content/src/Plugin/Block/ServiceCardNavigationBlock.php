bbdaf325e933b2fb0f8d0bc0f68f4567cc262d33
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2020-04-20 18:21:41 +0300

    Added service card navigation block.

---
+++ public/modules/custom/pako_content/src/Plugin/Block/ServiceCardNavigationBlock.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\pako_content\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'ServiceCardNavigationBlock' block.
+ *
+ * @Block(
+ *  id = "service_card_navigation_block",
+ *  admin_label = @Translation("Service card - Navigation Block"),
+ * )
+ */
+class ServiceCardNavigationBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+
+    if ($node = \Drupal::routeMatch()->getParameter('node')) {
+      if ($node->getType() == 'service_card') {
+        $config = \Drupal::service('config.factory')
+          ->get('pako_content.service_card_navigation')
+          ->get('pako_fields');
+        $fields = reset($config);
+
+        foreach ($fields as $icon => $field) {
+          if ($node->hasField($field) && !$node->get($field)->isEmpty()) {
+            $build['#items'][] = [
+              '#label' => $node->get($field)->getFieldDefinition()->getLabel(),
+              '#label_icon' => $icon,
+              '#theme' => 'pako_content_service_card_navigation_field',
+            ];
+          }
+        }
+        $build['#theme'] = 'pako_content_service_card_navigation';
+      }
+    }
+    return $build;
+  }
+
+}
